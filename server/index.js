c6c147444f04d81a4fd6a5d7ca6ba249e1b2beeb
kaiser <tandatvok16@gmail.com>
2023-08-06 13:42:25 +0700

    add : complete server, add client

--- server/index.js
+++ server/index.js
@@ -6,20 +6,22 @@ app.use(express.json());
 app.use(cors({ origin: true }));
 
 
-try {
-    const r = await axios.put(
-        "https://api.chatengine.io/users/",
-        { username: username, secret: username, first_name: username },
-        { headers: { "Private-Key": "XXXX" } }
-    );
-    return res.status(r.status).json(r.data);
-} catch (e) {
-    return res.status(e.response.status).json(e.response.data);
-}
+
+
+const axios = require("axios");
 
 app.post("/auth", async (req, res) => {
     const { username } = req.body;
-    return res.json({ username: username, secret: "sha256..." });
+    // Get or create user on Chat Engine!
+    try {
+        const r = await axios.put(
+            "https://api.chatengine.io/users/",
+            { username: username, secret: username, first_name: username },
+            { headers: { "Private-Key": "58a57eb1-b843-4b2c-b055-0b9629003942" } }
+        );
+        return res.status(r.status).json(r.data);
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data);
+    }
 });
-
 app.listen(3000, () => console.log("Server running on port 3000"));
