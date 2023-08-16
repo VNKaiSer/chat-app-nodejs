8369164d3419311fc9a817088e6dcff88b00cb2f
kaiser <tandatvok16@gmail.com>
2023-08-16 21:22:52 +0700

    complete app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import AuthPage from './components/AuthPage'
+import ChatsPage from './components/ChatPage'
 
 function App() {
   const [user, setUser] = useState(undefined)
-  return (
-    <>
-      <AuthPage onAuth={(user) => { setUser(user) }} />
-    </>
-  )
+
+  if (!user) {
+    return <AuthPage onAuth={(user) => { setUser(user) }} />
+  }
+  return <ChatsPage user={user} />
+
 }
 
 export default App
